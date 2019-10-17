03a04d2c6dfbae4e3eb432d5f46ace5461180335
Thais Zorawski <thaiszorawski@gmail.com>
2019-10-17 11:45:02 -0300

    Grafo completo

--- yacc_tzora.py
+++ yacc_tzora.py
@@ -10,116 +10,144 @@ from anytree.exporter import DotExporter
 # Get the token map from the lexer.  This is required.
 from lex_tzora import tokens
 
-
+count = 0
 
 def p_programa (p):
-	'''programa : lista_declaracoes'''
-	p[0] = Node("programa", children=[p[1]])
-	# Gera grafo
-	DotExporter(p[0]).to_picture("grafo3.png")
+  '''programa : lista_declaracoes'''
+  global count
+  count += 1
+  print(count)
+  p[0] = Node("programa", children=[p[1]])
+  # Gera grafo
+  DotExporter(p[0]).to_picture("grafo3.png")
 
 def p_lista_declaracoes (p):
     '''lista_declaracoes : lista_declaracoes declaracao
                          | declaracao '''
-    #print("Declaracao: " + str(p[1].height))
+    global count
+    count += 1
+    #print("Declaracao: " + count)
     if (len(p) == 2):
-        p[0] = Node("lista_declaracoes/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("lista_declaracoes/" + str(count), children=[p[1]])
     elif (len(p) == 3):
-        p[0] = Node("lista_declaracoes/" + str(p[1].height), children=[p[1], p[2]])
+        p[0] = Node("lista_declaracoes/" + str(count), children=[p[1], p[2]])
 
 def p_declaracao (p):
     '''declaracao : declaracao_variaveis
                   | inicializacao_variaveis
                   | declaracao_funcao
     '''
-    p[0] = Node("declaracao/" + str(p[1].height), children=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("declaracao/" + str(count), children=[p[1]])
 
 def p_declaracao_variaveis (p):
     '''declaracao_variaveis : tipo DOISPONTOS lista_variaveis'''
-    p[0] = Node("declaracao_variaveis/" + str(p[1].height), children=[p[1], p[3]])
+    global count
+    count += 1
+    p[0] = Node("declaracao_variaveis/" + str(count), children=[p[1], p[3]])
 
 def p_inicializacao_variaveis (p):
     'inicializacao_variaveis : atribuicao'
-    p[0] = Node("inicializacao_variaveis/" + str(p[1].height), children=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("inicializacao_variaveis/" + str(count), children=[p[1]])
     #p[0] = p[1]
 
 def p_lista_variaveis (p):
     '''lista_variaveis : lista_variaveis "," var 
                        | var
     '''
+    global count
+    count += 1
     if (len(p) == 4):
-
-        p[0] = Node("lista_variaveis/" + str(p[1].height), children=[p[1], p[3]])
+        p[0] = Node("lista_variaveis/" + str(count), children=[p[1], p[3]])
     elif (len(p) == 2):
-        p[0] = Node("lista_variaveis/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("lista_variaveis/" + str(count), children=[p[1]])
 
 def p_var (p):
     '''var : ID
            | ID indice
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("var " + p[1], valor=[p[1]])
+        p[0] = Node("var " + p[1] + "/" + str(count), valor=[p[1]])
     elif (len(p) == 3):
-        p[0] = Node("var/" + str(p[1].height), children=[p[2]], valor=[p[1]])
+        p[0] = Node("var/" + str(count), children=[p[2]], valor=[p[1]])
 
 def p_indice (p):
     '''indice : indice ECOLCHE expressao DCOLCHE
               | ECOLCHE expressao DCOLCHE
     '''
+    global count
+    count += 1
     if (len(p) == 5):
-        p[0] = Node("indice/" + str(p[1].height), children=[p[1], p[3]])
+        p[0] = Node("indice/" + str(count), children=[p[1], p[3]])
     elif (len(p) == 4):
-        p[0] = Node("indice/" + str(p[2].height), children=[p[2]])
+        p[0] = Node("indice/" + str(count), children=[p[2]])
 
 def p_tipo (p):
     '''tipo : INTEIRO
             | FLUTUANTE
     '''
-    p[0] = Node("tipo " + p[1], valor=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("tipo " + p[1] + "/" + str(count), valor=[p[1]])
 
 def p_declaracao_funcao (p):
     '''declaracao_funcao : tipo cabecalho 
 												 | cabecalho
     '''
+    global count
+    count += 1
     if (len(p) == 3):
-        p[0] = Node("declaracao_funcao/" + str(p[1].height), children=[p[1], p[2]])
+        p[0] = Node("declaracao_funcao/" + str(count), children=[p[1], p[2]])
     elif (len(p) == 2):
-        p[0] = Node("declaracao_funcao/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("declaracao_funcao/" + str(count), children=[p[1]])
 
 def p_cabecalho (p):
     '''cabecalho : ID EPAREN lista_parametros DPAREN corpo FIM
     '''
-    p[0] = Node("cabecalho/" + str(p[3].height), children=[p[3], p[5]], valor=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("cabecalho/" + str(count), children=[p[3], p[5]], valor=[p[1]])
 
 def p_lista_parametros (p):
     '''lista_parametros : lista_parametros VIRGULA parametro
 												| parametro
 												| vazio
     '''
-    print("Lista variaveis: ")
-    print(len(p))
+    global count
+    count += 1
+    #print("Lista variaveis: ")
+    #print(len(p))
     if (len(p) == 4):
-        p[0] = Node("lista_parametros/" + str(p[1].height), children=[p[1]], valor=[p[3]])
+        p[0] = Node("lista_parametros/" + str(count), children=[p[1]], valor=[p[3]])
     elif (len(p) == 2):
-        p[0] = Node("lista_parametros", valor=[p[1]])
+        p[0] = Node("lista_parametros/" + str(count), valor=[p[1]])
 
 def p_parametro (p):
     '''parametro : tipo DOISPONTOS ID
 								 |  parametro ECOLCHE DCOLCHE
     '''
+    global count
+    count += 1
     if (p[2] == "DOISPONTOS"):
-        p[0] = Node("parametro/" + str(p[1].height), children=[p[1]], valor=[p[3]])
+        p[0] = Node("parametro/" + str(count), children=[p[1]], valor=[p[3]])
     elif (p[2] == "ECOLCHE"):
-        p[0] = Node("parametro/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("parametro/" + str(count), children=[p[1]])
 
 def p_corpo (p):
     '''corpo : corpo acao 
 						 | vazio
     '''
+    global count
+    count += 1
     if (len(p) == 3):
-        p[0] = Node("corpo/" + str(p[1].height), children=[p[1], p[2]])
+        p[0] = Node("corpo/" + str(count), children=[p[1], p[2]])
     elif (len(p) == 2):
-        p[0] = Node("corpo", valor=[p[1]])
+        p[0] = Node("corpo/" + str(count), valor=[p[1]])
 
 def p_acao (p):
     '''acao : expressao
@@ -131,100 +159,126 @@ def p_acao (p):
 						| RETORNA
 						| error
     '''
+    global count
+    count += 1
     #print("ACAo")
     #print(type(p[1]))
     #print(p[1])
     if (type(p[1]) is str):
         #print("é string")
-        p[0] = Node("acao " + p[1], valor=[p[1]])
+        p[0] = Node("acao " + p[1] + "/" + str(count), valor=[p[1]])
     else:
-        p[0] = Node("acao/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("acao/" + str(count), children=[p[1]])
 
 def p_se (p):
     '''se : SE expressao ENTAO corpo FIM
 					| SE expressao ENTAO corpo SENAO corpo FIM
     '''
+    global count
+    count += 1
     if (len(p) == 6):
-        p[0] = Node("se/" + str(p[2].height), children=[p[2], p[4]])
+        p[0] = Node("se/" + str(count), children=[p[2], p[4]])
     elif (len(p) == 8):
-        p[0] = Node("se/" + str(p[2].height), children=[p[2], p[4], p[6]])
+        p[0] = Node("se/" + str(count), children=[p[2], p[4], p[6]])
 
 def p_repita (p):
     '''repita : REPITA corpo ATE expressao
     '''
-    p[0] = Node("repita/" + str(p[2].height), children=[p[2], p[4]])
+    global count
+    count += 1
+    p[0] = Node("repita/" + str(count), children=[p[2], p[4]])
 
 def p_atribuicao (p):
     '''atribuicao : var ATRIBUICAO expressao
     '''
-    p[0] = Node("atribuicao/" + str(p[1].height), children=[p[1], p[3]])
+    global count
+    count += 1
+    p[0] = Node("atribuicao/" + str(count), children=[p[1], p[3]])
 
 def p_leia (p):
     '''leia : LEIA EPAREN var DPAREN
     '''
-    p[0] = Node("leia/" + str(p[3].height), children=[p[3]])
+    global count
+    count += 1
+    p[0] = Node("leia/" + str(count), children=[p[3]])
 
 def p_escreva (p):
     '''escreva : ESCREVA EPAREN expressao DPAREN
     '''
-    p[0] = Node("escreva/" + str(p[3].height), children=[p[3]])
+    global count
+    count += 1
+    p[0] = Node("escreva/" + str(count), children=[p[3]])
 
 def p_retorna (p):
     '''retorna : RETORNA EPAREN expressao DPAREN
     '''
-    p[0] = Node("retorna/" + str(p[3].height), children=[p[3]])
+    global count
+    count += 1
+    p[0] = Node("retorna/" + str(count), children=[p[3]])
 
 def p_expressao (p):
     '''expressao : expressao_logica
 								 | atribuicao
     '''
-    p[0] = Node("expressao/" + str(p[1].height), children=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("expressao/" + str(count), children=[p[1]])
 
 def p_expressao_logica (p):
     '''expressao_logica : expressao_simples
 												| expressao_logica operador_logico expressao_simples
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("expressao_logica/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("expressao_logica/" + str(count), children=[p[1]])
     elif (len(p) == 4):
-        p[0] = Node("expressao_logica/" + str(p[1].height), children=[p[1], p[2], p[3]])
+        p[0] = Node("expressao_logica/" + str(count), children=[p[1], p[2], p[3]])
 
 def p_expressao_simples (p):
     '''expressao_simples : expressao_aditiva
 												 | expressao_simples operador_relacional expressao_aditiva
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("expressao_simples/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("expressao_simples/" + str(count), children=[p[1]])
     elif (len(p) == 4):
-        p[0] = Node("expressao_simples/" + str(p[1].height), children=[p[1], p[2], p[3]])
+        p[0] = Node("expressao_simples/" + str(count), children=[p[1], p[2], p[3]])
 
 def p_expressao_aditiva (p):
     '''expressao_aditiva : expressao_multiplicativa
 												 | expressao_aditiva operador_soma expressao_multiplicativa
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("expressao_aditiva/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("expressao_aditiva/" + str(count), children=[p[1]])
     elif (len(p) == 4):
-        p[0] = Node("expressao_aditiva/" + str(p[1].height), children=[p[1], p[2], p[3]])
+        p[0] = Node("expressao_aditiva/" + str(count), children=[p[1], p[2], p[3]])
 
 def p_expressao_multiplicativa (p):
     '''expressao_multiplicativa : expressao_unaria
 								| expressao_multiplicativa operador_multiplicacao expressao_unaria
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("expressao_multiplicativa/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("expressao_multiplicativa/" + str(count), children=[p[1]])
     elif (len(p) == 4):
-        p[0] = Node("expressao_multiplicativa/" + str(p[1].height), children=[p[1], p[2], p[3]])
+        p[0] = Node("expressao_multiplicativa/" + str(count), children=[p[1], p[2], p[3]])
 
 def p_expressao_unaria (p):
     '''expressao_unaria : fator
                         | operador_soma fator
                         | operador_negacao fator
     '''
+    global count
+    count += 1
     if (len(p) == 2):
-        p[0] = Node("expressao_unaria/" + str(p[1].height), children=[p[1]])
+        p[0] = Node("expressao_unaria/" + str(count), children=[p[1]])
     elif (len(p) == 3):
-        p[0] = Node("expressao_unaria/" + str(p[1].height), children=[p[1], p[2]])
+        p[0] = Node("expressao_unaria/" + str(count), children=[p[1], p[2]])
 
 #??
 def p_operador_relacional (p):
@@ -235,7 +289,7 @@ def p_operador_relacional (p):
                             | MENORIGUAL
                             | MAIORIGUAL
     '''
-    p[0] = Node("operador_relacional " + p[1], valor=[p[1]])
+    p[0] = Node("operador_relacional " + p[1] + "/" + str(count), valor=[p[1]])
 
 def p_operador_soma (p):
     '''operador_soma : ADICAO
@@ -266,10 +320,12 @@ def p_fator (p):
 						 | chamada_funcao
 						 | numero
     '''
+    global count
+    count += 1
     if (len(p) == 4):
-        p[0] = Node("fator", children=[p[2]])
+        p[0] = Node("fator/" + str(count), children=[p[2]])
     elif (len(p) == 2):
-        p[0] = Node("fator", children=[p[1]])
+        p[0] = Node("fator/" + str(count), children=[p[1]])
 
 #??
 def p_numero (p):
@@ -283,17 +339,21 @@ def p_numero (p):
 def p_chamada_funcao (p):
     '''chamada_funcao : ID EPAREN lista_argumentos DPAREN
     '''
-    p[0] = Node("chamada_funcao", children=[p[3]], valor=[p[1]])
+    global count
+    count += 1
+    p[0] = Node("chamada_funcao/" + str(count), children=[p[3]], valor=[p[1]])
 
 def p_lista_argumentos (p):
     '''lista_argumentos : lista_argumentos VIRGULA expressao
 												| expressao
 												| vazio
     '''
+    global count
+    count += 1
     if (len(p) == 4):
-        p[0] = Node("lista_argumentos", children=[p[1], p[3]])
+        p[0] = Node("lista_argumentos/" + str(count), children=[p[1], p[3]])
     elif (len(p) == 2):
-        p[0] = Node("lista_argumentos", children=[p[1]])
+        p[0] = Node("lista_argumentos/" + str(count), children=[p[1]])
 
 # Error rule for syntax errors
 def p_error(p):
